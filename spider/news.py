7f3d0e24b7cceef5c6f3f4b43aa335057f7afd4f
Leyuan <leyuan.ly@gmail.com>
2018-03-21 02:04:06 -0600

    wip

--- spider/news.py
+++ spider/news.py
@@ -1,42 +1,46 @@
 #encoding:UTF-8
 from bs4 import BeautifulSoup
 import urllib2
-import urllib
-import json 
-import sys
-import re
+
+from helper import SpiderHelper
+
+helper = SpiderHelper()
+
 def getPage(url):
     request = urllib2.Request(url)
     request.add_header('User-Agent','Mozilla/4.0 (compatible;MSIE 5.5; Windows NT)')
     response=urllib2.urlopen(request)
     return response.read()
 
-url="http://www.jinse.com/news"
+url = "http://www.jinse.com/news"
 
-result=getPage(url)
-#print result
-
-#title = re.findall('title',result.text,re.S)
-#print title
-
-
-bs=BeautifulSoup(result)
-
-[style.extract() for style in bs.findAll('style')]
-
+result = getPage(url)
+bs = BeautifulSoup(result, "html.parser")
 bs.prettify()
-[s.extract() for s in bs(['span', 'new'])]
 
-#m=re.match(r'var .*?location', content)
-#print bs
-txt='./news.html'
-f = open(txt,"w+")  
-f.write('<html>'+'<meta http-equiv="Content-Type" content="text/html; charset=utf-8">')
-counter=0
-for k in bs.find_all('div',class_='news'):
-    print(k)
-    f.write(str(k))
+news_html = '../news.html'
+f = open(news_html, "w")
 
-    
+f.write(helper.create_header())
 
-f.write('</html>')    
+for entry in bs.find_all('div', class_='list'):
+    entry['class'] = 'entry entry-small'
+
+    [image_div, message_div] = entry.find_all('div', recursive=False)
+    image_div['class'] = 'thumbnail-attachment'
+    message_div['class'] = 'entry-body'
+
+    del image_div.a['class']
+    del image_div.img['height']
+    del image_div.img['border']
+    image_div.img['width'] = '400'
+
+    [m_title, m_body, m_tips] = message_div.find_all('div', recursive=False)
+    m_title['class'] = 'entry-title'
+    m_body.name = 'p'
+    del m_body['class']
+    del m_tips['class']
+
+    f.write(str(entry))
+
+f.write(helper.create_footer())
