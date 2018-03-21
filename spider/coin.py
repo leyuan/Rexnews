132bf69b80f685256b2c4a79ceb98f40d1267a4b
LingTian <danjinxiangsi@gmail.com>
2018-03-21 11:29:49 +0800

    信息栏
    1

---
+++ spider/coin.py
@@ -0,0 +1,48 @@
+#encoding:UTF-8
+from bs4 import BeautifulSoup
+import urllib2
+import urllib
+import json 
+import sys
+import re
+def getPage(url):
+    request = urllib2.Request(url)
+    request.add_header('User-Agent','Mozilla/4.0 (compatible;MSIE 5.5; Windows NT)')
+    response=urllib2.urlopen(request)
+    return response.read()
+
+url="http://www.jinse.com/coin"
+
+result=getPage(url)
+#print result
+
+#title = re.findall('title',result.text,re.S)
+#print title
+
+
+bs=BeautifulSoup(result)
+
+[style.extract() for style in bs.findAll('style')]
+
+bs.prettify()
+[s.extract() for s in bs(['span', 'new'])]
+
+#m=re.match(r'var .*?location', content)
+#print bs
+txt='./coins.html'
+f = open(txt,"w+")  
+f.write('<html>'+'<meta http-equiv="Content-Type" content="text/html; charset=utf-8">')
+counter=0
+
+for k in bs.find_all('div',class_='wrap bsummary'):
+    print(k)
+    f.write(str(k))
+
+for k in bs.find_all('div',class_='link'):
+    print(k)
+    f.write(str(k))
+    f.write("<br>")
+
+    
+
+f.write('</html>')    
