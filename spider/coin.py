56103017059783a4df3664bd574e3febe5fc115d
Leyuan <leyuan.ly@gmail.com>
2018-03-22 13:25:29 -0600

    fix links for lives page

--- spider/coin.py
+++ spider/coin.py
@@ -1,48 +1,68 @@
 #encoding:UTF-8
 from bs4 import BeautifulSoup
 import urllib2
-import urllib
-import json 
-import sys
-import re
+
+def create_header():
+    return """
+    <table>
+        <tbody>
+            <tr>
+                <th>h1</th>
+                <th>h2</th>
+                <th>h3</th>
+                <th>h4</th>
+                <th>h5</th>
+                <th>h6</th>
+                <th>h7</th>
+                <th>h8</th>
+            </tr>
+    """
+
+def create_footer():
+    return """
+        </tbody>
+    </table>
+    """
+
+
 def getPage(url):
     request = urllib2.Request(url)
     request.add_header('User-Agent','Mozilla/4.0 (compatible;MSIE 5.5; Windows NT)')
     response=urllib2.urlopen(request)
     return response.read()
 
-url="http://www.jinse.com/coin"
 
-result=getPage(url)
-#print result
+url = "http://www.jinse.com/coin"
 
-#title = re.findall('title',result.text,re.S)
-#print title
+result = getPage(url)
 
-
-bs=BeautifulSoup(result)
+bs=BeautifulSoup(result, "html.parser")
 
 [style.extract() for style in bs.findAll('style')]
 
 bs.prettify()
 [s.extract() for s in bs(['span', 'new'])]
 
-#m=re.match(r'var .*?location', content)
-#print bs
 txt='./coins.html'
-f = open(txt,"w+")  
-f.write('<html>'+'<meta http-equiv="Content-Type" content="text/html; charset=utf-8">')
-counter=0
+f = open(txt, "w")
+f.write(create_header())
 
-for k in bs.find_all('div',class_='wrap bsummary'):
-    print(k)
-    f.write(str(k))
+# TODO: Header Summary
+# for k in bs.find_all('div',class_='wrap bsummary'):
+#     print(k)
+#     f.write(str(k))
 
 for k in bs.find_all('div',class_='link'):
-    print(k)
+    for ul in k.find_all('ul'):
+        del ul['class']
+        ul.name = "tr"
+
+        for li in ul.find_all('li'):
+            del li['class']
+            li.name = "td"
+
     f.write(str(k))
-    f.write("<br>")
 
-    
 
-f.write('</html>')    
+
+f.write(create_footer())
